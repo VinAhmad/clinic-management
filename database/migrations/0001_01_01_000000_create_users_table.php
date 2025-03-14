e899bf7ffff5b56b48dc0f54025d574440187816
VinAhmad <kev.af18@gmail.com>
2025-03-14 13:07:09 +0700

    connect sqlite dan membuat migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,11 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'doctor', 'patient']);
+            $table->string('phone')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
+            $table->text('address')->nullable();
+            $table->string('specialization')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
